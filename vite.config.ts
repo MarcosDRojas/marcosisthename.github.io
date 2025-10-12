6ebffca96f80ee156b35fd4e55e2351d7fe1613a
marcos rojas <marcos.rojas24@gmail.com>
2025-10-12 19:30:50 -0400

    updating base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/marcosisthename.github.io/', 
   plugins: [
     vue(),
     vueJsx(),
