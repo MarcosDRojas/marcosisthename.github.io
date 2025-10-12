939941742301cfd124ebb4f14941d34aceff4b83
marcos rojas <marcos.rojas24@gmail.com>
2025-10-12 18:27:38 -0400

    trying to make the webpage be seen

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
